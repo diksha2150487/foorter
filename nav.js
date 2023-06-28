58194b6c79ad776a8ba1aff648ad225d447f0bef
diksha2150487 <130853102+diksha2150487@users.noreply.github.com>
2023-06-28 22:04:52 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,58 @@
+console.log("hlw");
+let nav = document.querySelector("#nav");
+let template = document.createElement("template");
+template.innerHTML = `<header>
+<section class="navigation">
+  <div class="nav-container">
+    <div class="brand">
+      <a><img src="./images/new.jpeg" alt="logo" class="nav_logo" /></a>
+    </div>
+    <nav>
+      <div class="nav-mobile">
+        <a id="navbar-toggle" href="#!"><span></span></a>/
+      </div>
+      <ul class="nav-list">
+        <li>
+          <a href="#!" id="home">HOME</a>
+        </li>
+        <li>
+          <a href="#!">ABOUT US</a>
+        </li>
+        <li>
+          <a href="#!">PRODUCTS</a>
+          <ul class="navbar-dropdown">
+            <li>
+              <a href="#!">Service</a>
+            </li>
+            <li>
+              <a href="#!">CONSTUANTY</a>
+            </li>
+            <li>
+              <a href="#!">Stylus</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#!">SERVICES</a>
+          <ul class="navbar-dropdown">
+            <li>
+              <a href="#!">Sass</a>
+            </li>
+            <li>
+              <a href="#!">Less</a>
+            </li>
+            <li>
+              <a href="#!">Stylus</a>
+            </li>
+          </ul>
+        </li>
+        <li class="side_btn">
+          <a href="#!">CONSULTANCY <i class="fa-solid fa-arrow-right"></i></a>
+        </li>
+    </nav>
+  </div>
+</section>
+
+</header>
+`;
+nav.appendChild(template.content);
